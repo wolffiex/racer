b13c9429c674f97b0dcc76d5935301bf27909ef6
Adam Wolff <wolffiex@fb.com>
2016-02-13 10:29:29 -0800

    Center sprite

--- src/sprite.js
+++ src/sprite.js
@@ -1,11 +1,14 @@
 export class Sprite {
   constructor(src) {
     var img = new Image();
+    this.width = 0;
+    this.height = 0;
+    img.onload = () => {this.width = img.width; this.height = img.height;};
     img.src = src
     this.img = img;
   }
 
   draw(context){
-    context.drawImage(this.img, 0, 0);
+    context.drawImage(this.img, -this.width/2, -this.height/2);
   }
 };
